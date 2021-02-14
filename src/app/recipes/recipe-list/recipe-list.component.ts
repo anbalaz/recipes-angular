117b685a3fa89124a494904bc282e9e2813c8f8e
anbalaz <anbalaz@gmail.com>
2021-02-14 09:26:21 +0100

    Adding ingredient model, updating html templates with bootstrap and *ngFor.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,10 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
+      'https://cookingpotsnpans.com/wp-content/uploads/2020/09/london-broil.jpg'),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
       'https://cookingpotsnpans.com/wp-content/uploads/2020/09/london-broil.jpg')
   ];
 
