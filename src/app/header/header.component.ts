e1d249ba0c3ab24c6f73bbfb5d16b97b42de51ca
anbalaz <anbalaz@gmail.com>
2021-04-13 20:44:40 +0200

    Add base routing to project.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,14 +6,9 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
   collapsed = true;
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
   }
 }
