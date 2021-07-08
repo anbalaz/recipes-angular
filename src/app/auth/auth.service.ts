b9a5b50b7d30aaab020f4144c61f485892dc7505
anbalaz <anbalaz@gmail.com>
2021-07-08 22:29:19 +0200

    Adding auth.service.ts with signUp method.

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+  expires_in: string;
+  token_type: string;
+  refresh_token: string;
+  id_token: string;
+  user_id: string;
+  project_id: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private signUpUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=';
+  private webApiKey = 'AIzaSyBvCWI1vHxBTmXZTHc_VRww1l6tZgRmeJs';
+
+  constructor(private http: HttpClient) { }
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(this.signUpUrl + this.webApiKey,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
+}
